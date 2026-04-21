4453fa355d68efc47a3efa7240f0b89ee1859cba
mohammedtaufeeqfb-ux <mohammedtaufeeqfb@gmail.com>
2026-04-21 09:33:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("🚀 New Change DevOps Pipeline!");
 });
 
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log("webhook test");
 });
